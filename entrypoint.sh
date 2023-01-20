19b16b0327869a4937f6c9665efbbd3d1cfc0b4c
Brian Morrison II <brian@brianmorrison.me>
2023-01-20 11:07:20 -0500

    update script

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@
 # time=$(date)
 # echo "time=$time" >> $GITHUB_OUTPUT
 
-pscale branch create $1 $2
+pscale branch create $1 $2 --service-token $PLANETSCALE_TOKEN --service-token-id $PLANETSCALE_TOKEN_NAME
