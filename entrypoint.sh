59cead46895ca0f4e96fe46700cfeb294181a014
Brian Morrison II <brian@brianmorrison.me>
2023-01-20 11:10:05 -0500

    add org_name

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@
 # time=$(date)
 # echo "time=$time" >> $GITHUB_OUTPUT
 
-pscale branch create $1 $2 --service-token $PLANETSCALE_TOKEN --service-token-id $PLANETSCALE_TOKEN_NAME
+pscale branch create $1 $2 --org $3 --service-token $PLANETSCALE_TOKEN --service-token-id $PLANETSCALE_TOKEN_NAME
